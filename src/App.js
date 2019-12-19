5d6e593a6ffd537bbfe0ca4892ed661ef05f4d83
Liam Hockley <xdega121@gmail.com>
2019-12-19 17:58:17 -0600

    Add Home component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PostList from "./components/posts/PostList"
+import Home from "./components/Home"
 import Header from "./components/Header"
 import {
   BrowserRouter as Router,
@@ -7,34 +8,20 @@ import {
   Route,
 } from "react-router-dom";
 
-function Home() {
-    return (
-        <div className="m-8">Welcome!</div>
-    )
-}
-
-function Posts() {
-    const POST_URL = 'http://drupal.docker.localhost:8000/jsonapi/node/article?sort=-created,title&include=field_tags';
-    return (
-        <div>
-            <PostList url={POST_URL} />
-        </div>
-    )
-}
-
 // Begin routing here
 export default function main() {
+  const POST_URL = 'http://drupal.docker.localhost:8000/jsonapi/node/article?sort=-created,title&include=field_tags';
+  
   return (
     <Router>
       <div className='m-8'>
         <Header />
-        {/* Content Area. Refactor? */}
         <Switch>
           <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/posts">
-            <Posts />
+          <Route exact path="/posts">
+            <PostList url={POST_URL} />
           </Route>
         </Switch>
       </div>
